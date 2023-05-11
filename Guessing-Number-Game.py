ba864f03df2312ef5cf89695f075c9f083a9f8f1
vinegm <117782568+vinegm@users.noreply.github.com>
2023-05-11 11:54:55 -0300

    Leaderboard Frame
    Changed PB to Score, made a exoskeleton for the leaderboard frame, and fixing to the db

--- Guessing-Number-Game.py
+++ Guessing-Number-Game.py
@@ -24,7 +24,8 @@ def create_leaderboard(connection):
     cursor.execute("""CREATE TABLE IF NOT EXISTS leaderboard (
                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                    player VARCHAR NOT NULL UNIQUE,
-                   score INTEGER NOT NULL
+                   score INTEGER NOT NULL,
+                   consecutive_wins INTEGER NOT NULL
                    )""")
     cursor.close()
 
@@ -35,7 +36,7 @@ class GuessingNumberGame(tk.Tk):
         tk.Tk.__init__(self)
 
         self.title("Numdle (Guest)")
-        self.geometry("400x400")
+        self.geometry("360x360")
         self.resizable(False, False)
         self.eval("tk::PlaceWindow . center")
         self.iconbitmap("assets/Icon.ico")
@@ -46,11 +47,21 @@ class GuessingNumberGame(tk.Tk):
         frames_holder = tk.Frame(self)
         frames_holder.pack(anchor = "center")
 
-        game_frame = Game(leaderboard, frames_holder, self)
-        game_frame.grid(row = 0,
-                        column = 0,
-                        sticky = "nsew")
+        self.frames = {}
+        for f in (Game, Leaderboard):
+            frame_name = f.__name__
+            frame = f(leaderboard, frames_holder, self)
+            frame.grid(row = 0,
+                            column = 0,
+                            sticky = "nsew")
+            self.frames[frame_name] = frame
 
+        self.change_frame("Game")
+        
+    def change_frame(self, selected_frame):
+        next_frame = self.frames[selected_frame]
+        next_frame.tkraise()
+        
 
 class Game(tk.Frame):
     """Frame where you can play and guess"""
@@ -73,14 +84,16 @@ class Game(tk.Frame):
         profile_image.thumbnail((30, 30))
         profile_image = ImageTk.PhotoImage(profile_image)
 
-        self.player = [None, "Guest", 0]
+        self.player = [None, "Guest", 0, 0]
+        self.score = 0
+        self.consecutive_wins = 0
         profile = tk.Label(leaderboard_profile,
                            image = profile_image)
         profile.image = profile_image
         profile.grid(row = 0,
-                     column = 0,
+                     column = 1,
                      sticky = "e")
-        profile.bind("<Button-1>", lambda event: self._select_player(simpledialog.askstring("Who are you?", f"Currently playing as: {self.player[1]}\nType a different nickname to change player:")))
+        profile.bind("<Button-1>", lambda event: self._select_player(simpledialog.askstring("Who are you?", f"Currently playing as: {self.player[1]}\nType a different nickname to change player:"), rows))
 
         leaderboard_image = Image.open("assets/Leaderboard.png")
         leaderboard_image.thumbnail((30, 30))
@@ -90,8 +103,9 @@ class Game(tk.Frame):
                                     image = leaderboard_image)
         open_leaderboard.image = leaderboard_image
         open_leaderboard.grid(row = 0,
-                              column = 1,
+                              column = 0,
                               sticky = "w")
+        open_leaderboard.bind("<Button-1>", lambda event: window.change_frame("Leaderboard"))
 
         header = tk.Label(header_holder,
                           text = "Numdle",
@@ -102,21 +116,20 @@ class Game(tk.Frame):
                     sticky = "nsew")
         
         self.personal_best = tk.Label(header_holder,
-                                      text = f"PB: {self.player[2]}",
+                                      text = f"Score: {self.player[2]}",
                                       font = ("Arial", 12, "bold"))
         self.personal_best.grid(row = 0,
                                 column = 2,
                                 sticky = "e")
 
         self.info = tk.Label(self,
-                             text = "",
+                             text = "Are you a good guesser?",
                              font = ("Arial", 12))
         self.info.pack(anchor = "center")
 
         random_number = generate_number()
         rows = self._create_boxes(master)
 
-        self.consecutive_wins = 0
         self.guess_row = 0
         self.guess_button = tk.Button(self,
                                  text = "Guess",
@@ -128,7 +141,7 @@ class Game(tk.Frame):
         for box in rows[self.guess_row]:
             if box.get() == "":
                 messagebox.showwarning(title = "Fill the Boxes",
-                                     message = "You have to fill all the\nboxes to make a guess!")
+                                       message = "You have to fill all the\nboxes to make a guess!")
                 return
         _found = -1
 
@@ -141,14 +154,24 @@ class Game(tk.Frame):
                 box.configure(bg = "Gray")
         
         if np.all(target == _found):
-            self._update_boxes(rows, False)
             self.guess_button.config(text = "Play Again",
                                      command = lambda: self._clear_boxes(rows))
             self.info.configure(text = "Nice, you got it!")
             self.consecutive_wins += 1
-            if self.consecutive_wins > self.player[2]:
-                self.player[2] = self.consecutive_wins
-                self.personal_best.configure(text = f"PB: {self.player[2]}")
+
+            if self.guess_row == 0:
+                self.score += 5
+            elif self.guess_row == 1:
+                self.score += 4
+            elif self.guess_row == 2:
+                self.score += 3
+            elif self.guess_row == 3:
+                self.score += 2
+            elif self.guess_row == 4:
+                self.score += 1
+
+            self.personal_best.configure(text = f"Score: {self.score}")
+            self._update_boxes(rows, False)
             return
 
         for box in rows[self.guess_row]:
@@ -163,8 +186,8 @@ class Game(tk.Frame):
                                      command = lambda: self._clear_boxes(rows))
             self.info.configure(text = f"The number was {''.join(map(str, random_number))}")
             self.consecutive_wins = 0
-            if self.player[1] != "Guest":
-                self._update_PB()
+            if self.player[1] != "Guest" and self.player[1] != "guest":
+                self._update_status()
             self._update_boxes(rows, False)
         else:
             self._update_boxes(rows)
@@ -268,11 +291,11 @@ class Game(tk.Frame):
         
         return False
 
-    def _select_player(self, player):
+    def _select_player(self, player, rows):
         if player == None or player == "":
             return
         
-        self._update_PB()
+        self._update_status()
         cursor = self.connection.cursor()
         cursor.execute("""SELECT * FROM leaderboard WHERE player = ?""", (player,))
         result = cursor.fetchone()
@@ -284,28 +307,106 @@ class Game(tk.Frame):
         
         cursor.close()
         self.player = list(result)
-        self.personal_best.configure(text = f"PB: {self.player[2]}")
-        self.info.configure(text = "")
+
         self.window.title(f"Numdle ({self.player[1]})")
+        self.personal_best.configure(text = "Score: 0")
+
+        self.score = 0
+        self.consecutive_wins = 0
+
+        self._clear_boxes(rows)
+        self.info.configure(text = "")
 
     def _create_player(self, player):
         cursor = self.connection.cursor()
 
-        cursor.execute("INSERT INTO leaderboard (player, score) VALUES (?, ?)", (player, 0))
+        cursor.execute("INSERT INTO leaderboard (player, score, consecutive_wins) VALUES (?, ?, ?)", (player, 0, 0))
         self.connection.commit()
 
         cursor.close()
 
-    def _update_PB(self):
+    def _update_status(self):
         cursor = self.connection.cursor()
-        cursor.execute("""UPDATE leaderboard SET score = ? WHERE id = ?""", (self.player[2], self.player[0]))
+        if self.player[2] < self.score and self.player[3] < self.consecutive_wins:
+            cursor.execute("""UPDATE leaderboard SET score = ?, consecutive_wins = ?  WHERE id = ?""", (self.player[2], self.player[3], self.player[0]))
+        elif self.player[2] < self.score:
+            cursor.execute("""UPDATE leaderboard SET score = ?  WHERE id = ?""", (self.player[2], self.player[0]))
+        elif self.player[3] < self.consecutive_wins:
+            cursor.execute("""UPDATE leaderboard SET consecutive_wins = ?  WHERE id = ?""", (self.player[3], self.player[0]))
         self.connection.commit()
         cursor.close()
+
 
 class Leaderboard (tk.Frame):
     """Frame for displaying the top 10 best plays and a player best score"""
-    def __init__(self, master):
+    def __init__(self, connection, master, window):
         tk.Frame.__init__(self, master)
+        
+        header_holder= tk.Frame(self)
+        header_holder.pack(anchor = "center",
+                           fill = "x",
+                           pady = 5)
+        
+        back_image = Image.open("assets/Back.png")
+        back_image.thumbnail((30, 30))
+        back_image = ImageTk.PhotoImage(back_image)
+
+        back = tk.Label(header_holder,
+                        image = back_image)
+        back.image = back_image
+        back.pack(side = "left")
+        back.bind("<Button-1>", lambda event: window.change_frame("Game"))
+
+        header = tk.Label(header_holder,
+                          text = "Leaderboard:",
+                          font = ("Arial", 16, "bold"))
+        header.pack(side = "left",
+                    padx = 65)
+
+        leaderboard_holder = tk.Frame(self)
+        leaderboard_holder.pack(anchor = "center",
+                                fill = "both")
+        
+        self.build_leaderboard(leaderboard_holder)
+
+    def build_leaderboard(self, master):
+        master.grid_columnconfigure(0, minsize = 50)
+        master.grid_columnconfigure(list(range(1, 4)), minsize = 75)
+        rank_column = tk.Label(master,
+                               text = "Rank:",
+                               font = ("Arial", 12, "bold"),
+                               border = 1,
+                               relief = "solid")
+        rank_column.grid(row = 0,
+                         column = 0,
+                         sticky = "nsew")
+
+        player_column = tk.Label(master,
+                                 text = "Player:",
+                                 font = ("Arial", 12, "bold"),
+                                 border = 1,
+                                 relief = "solid")
+        player_column.grid(row = 0,
+                           column = 1,
+                           sticky = "nsew")
+        
+        consecutive_wins_column = tk.Label(master,
+                                           text = "Wins in a Row:",
+                                           font = ("Arial", 12, "bold"),
+                                           border = 1,
+                                           relief = "solid")
+        consecutive_wins_column.grid(row = 0,
+                                     column = 2,
+                                     sticky = "nsew")
+        
+        score_column = tk.Label(master,
+                                text = "Score:",
+                                font = ("Arial", 12, "bold"),
+                                border = 1,
+                                relief = "solid")
+        score_column.grid(row = 0,
+                          column = 3,
+                          sticky = "nsew")
 
 
 if __name__ == "__main__":
